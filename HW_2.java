2d28da984f8c8750380a3b8d51245bb13bfb7e0b
Jobega <37122818+JosephPobega@users.noreply.github.com>
2021-09-25 22:15:19 -0400

    Add files via upload
    Java program that calculates future invest value given the users investment amount, annual interest rate, and number of years

---
+++ HW_2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class HW_2 {
+
+	
+	
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		double AnnualInvestmentAmount, AnnualInterestRate, MonthlyInterestRate, years, fiv;
+		
+		System.out.println("Enter your Annual investment amount:");
+		AnnualInvestmentAmount = input.nextDouble();
+		System.out.println("Enter your Annual interest rate:");
+		AnnualInterestRate = input.nextDouble();
+		System.out.println("Enter the number of years:");
+		years = input.nextDouble();
+		MonthlyInterestRate = (AnnualInterestRate/1200);
+		fiv = AnnualInvestmentAmount * (Math.pow(1+MonthlyInterestRate,years*12));
+		System.out.println("your future investment is: " + "$" + fiv);
+	}
+
+}
