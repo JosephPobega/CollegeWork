da8681b3fd158c3984268977178c4204e34a2a09
Jobega <37122818+JosephPobega@users.noreply.github.com>
2021-10-13 07:55:18 -0400

    Add files via upload
    Have the user enter any letter A-Z. Then translate that letter and output the number on a phone keypad that maps to that letter.

---
+++ COMP-171/HW_4.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class HW_4 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		String letter;
+		int number;
+		System.out.println("type your letter:");
+		letter = input.nextLine();
+
+		if (letter == "A"){
+			System.out.print('1');
+		}
+		if (letter == "B") {
+			System.out.println('1');
+		
+		}
+		
+		
+	}
+}
