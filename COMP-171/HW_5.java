ac2fb8dc8433d6e4434ff62be8fba3200178f737
Jobega <37122818+JosephPobega@users.noreply.github.com>
2021-10-26 18:30:46 -0400

    Update HW_5.java
    fixing stuff

--- COMP-171/HW_5.java
+++ COMP-171/HW_5.java
@@ -1,23 +1,31 @@
+package hw_5_171;
+
 import java.util.Scanner;
-public class HW_5 {
+
+public class hw_5 {
 
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		Double loanAmount, InterestRate, monthlyPayment, totalPayment, MonthlyRate;
-		int numYears;
-		InterestRate = 5.0;
 		
-		System.out.println("Enter loan amount: ");
-		loanAmount = input.nextDouble();
-		System.out.println("Enter number of years: ");
-		numYears = input.nextInt();
-		
-		while(InterestRate <= .125) {
-			monthlyPayment = loanAmount * InterestRate  /  (1 - 1 / Math.pow(1 + InterestRate, numYears * 12)); 
-			totalPayment = monthlyPayment * numYears * 12;
-			MonthlyRate = InterestRate / 1200;
-			System.out.printf("%10.3f	%13.2f	%15.2f\n", monthlyPayment, totalPayment, MonthlyRate);
-		}
-	}
 
+			Scanner input = new Scanner(System.in);
+			Double loanAmount, InterestRate, monthlyPayment, totalPayment, MonthlyRate;
+			int numYears, counter;
+			InterestRate = 5.0;
+			counter = 0;
+			System.out.println("Enter loan amount: ");
+				loanAmount = input.nextDouble();
+				System.out.println("Enter number of years: ");
+				numYears = input.nextInt();
+				
+				while(counter <= numYears) {
+					InterestRate = InterestRate + .125;
+					monthlyPayment = loanAmount * InterestRate  /  (1 - 1 / Math.pow(1 + InterestRate, numYears * 12)); 
+					totalPayment = monthlyPayment * numYears * 12;
+					System.out.println("Interest Rate	Monthly Payment	  Total Payment");
+					System.out.printf("%10.3f	%13.2f	%15.2f\n", InterestRate, monthlyPayment, totalPayment);
+					counter++;
+				}
+			}
 }
+
+		
