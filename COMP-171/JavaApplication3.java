e155ed51c3dc758efc1ef1e1f9127727b85c6396
Jobega <37122818+JosephPobega@users.noreply.github.com>
2021-12-09 10:55:57 -0500

    Add files via upload
    homework I'm working on.

---
+++ COMP-171/JavaApplication3.java
@@ -0,0 +1,11 @@
+package classWork;
+import java.util.GregorianCalendar;
+public class JavaApplication3 {
+
+	public static void main(String[] args) {
+		GregorianCalendar mycal = new GregorianCalendar();
+		System.out.println("Month is: " + mycal.get(GregorianCalendar.MONTH));
+
+	}
+
+}
