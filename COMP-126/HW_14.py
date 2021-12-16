cfcc8ba2d9116e2c9fe51e4dd1ae931f0f6f7fc6
Jobega <37122818+JosephPobega@users.noreply.github.com>
2021-12-15 22:31:19 -0500

    Add files via upload
    Design a class that holds the following personal information: name, address, age, and phone number. Write appropriate accessor and mutator methods.
    
    Also design a program that creates three instances of the class.

---
+++ COMP-126/HW_14.py
@@ -0,0 +1,27 @@
+#Joseph Pobega
+#COMP 126
+
+
+class Personal:
+  def __init__(self, name, adress, age, phoneNumber):
+      self.name = name
+      self.adress = adress
+      self.age = age
+      self.phoneNumber = phoneNumber
+
+
+for i in range(3):
+    name = input("Name?")
+    adress = input("Adress?")
+    age = input("Age?")
+    phoneNumber = input("Phone number?")
+
+person = Personal(name, adress, age, phoneNumber)
+
+
+
+
+
+#Design a class that holds the following personal information: 
+# name, address, age, and phone number. Write appropriate accessor and mutator methods.
+#Also design a program that creates three instances of the class.
