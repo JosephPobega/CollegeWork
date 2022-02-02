0097b8ef5c176e1b12085911935f5f577bae3ec2
Jobega <37122818+JosephPobega@users.noreply.github.com>
2022-02-01 20:19:56 -0500

    Add files via upload
    Experimenting with world navigation with  arrays

---
+++ COMP-271/Blackmoor/Area.java
@@ -0,0 +1,19 @@
+public class Area{
+	private String description;
+	
+	public Area() {
+		description = "";
+	}
+	
+	public  Area(String desc) {
+		description = desc;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String desc) {
+		description = desc;
+	}
+}
