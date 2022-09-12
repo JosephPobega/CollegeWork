786ff616ccf7d569ae242def256cfb7a7207ff28
Jobega <37122818+JosephPobega@users.noreply.github.com>
2022-09-12 00:19:19 -0400

    Create Main.java

---
+++ CylinderCalculation/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+        
+
+    public static void main(String[] args) {
+       SISession2 session2 = new SISession2();
+        
+       System.out.println(session2.area());
+       System.out.println(session2.volume());
+        
+    }
+
+}
+
