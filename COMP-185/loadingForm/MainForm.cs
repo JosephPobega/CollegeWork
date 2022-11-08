399c54be4f6ae47112f003bc2e0968ec503c1d05
Jobega <37122818+JosephPobega@users.noreply.github.com>
2022-11-07 20:27:42 -0500

    Add files via upload

--- COMP-185/loadingForm/MainForm.cs
+++ COMP-185/loadingForm/MainForm.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -19,15 +19,29 @@ namespace WindowsFormsApp1
 
 
 
-
-
-
-     
-
         private void button1_Click(object sender, EventArgs e)
         {
             OtherForm formOther = new OtherForm();
-            formOther.Show();
+            formOther.ShowDialog();
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OtherForm2 formOther2 = new OtherForm2();
+            formOther2.ShowDialog();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            OtherForm3 formOther3 = new OtherForm3();
+            formOther3.ShowDialog();
         }
     }
 }
+
---
+++ COMP-185/loadingForm/OtherForm3.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class OtherForm3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(333, 289);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(156, 88);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "close";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(166, 133);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(217, 42);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "other form 3";
+            // 
+            // OtherForm3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "OtherForm3";
+            this.Text = "OtherForm3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
