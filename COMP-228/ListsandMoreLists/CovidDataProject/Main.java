d8e32b0ec3272090859c9030d88642afad59b3f0
Jobega <37122818+JosephPobega@users.noreply.github.com>
2023-02-14 00:55:28 -0500

    Update Main.java

--- COMP-228/ListsandMoreLists/CovidDataProject/Main.java
+++ COMP-228/ListsandMoreLists/CovidDataProject/Main.java
@@ -1,28 +1,33 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
-public class Main{
-	public static void main(String[] args) throws IOException {
-		String file = "src\\CovidData.csv";
-		BufferedReader reader = null;
-		String line = "";
-		
-		try {
-			reader = new BufferedReader(new FileReader(file));
-			while((line = reader.readLine()) != null) {
-				
-				String[] row = line.split(",");
-				
-				for(String index : row) {
-					System.out.printf("%5s", index);
-				}
-				System.out.println();
-			}
+public class Main {
+  public static void main(String[] args) throws IOException {
+    ArrayList<String[]> data = new ArrayList<>();
+
+    try (BufferedReader br = new BufferedReader(new FileReader("src\\CovidData.csv"))) {
+      String line;
+      while ((line = br.readLine()) != null) {
+        String[] row = line.split(",");
+        data.add(row);
+      }
+    }
+   
+    try (Scanner scanner = new Scanner(System.in)) {
+		System.out.println("USMER: 1, MEDICAL_UNIT: 2, SEX: 3, PATIENT_TYPE: 4, DATE_DIED, 5");
+		System.out.println("PRESS 0 TO EXIT");
+		int column = scanner.nextInt();
+		while(column != 0) {
+			System.out.println("USMER: 1, MEDICAL_UNIT: 2, SEX: 3, PATIENT_TYPE: 4, DATE_DIED, 5");
+			System.out.println("PRESS 0 TO EXIT");
+			int column1 = scanner.nextInt();
+		for (String[] row : data) {
+		  System.out.println(row[column1 - 1]);
 		}
-		catch(Exception e) {
-			e.printStackTrace();
-		}
-		finally {
-			reader.close();
-		}
+  }
 	}
+    }
 }
