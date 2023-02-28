09b7969bb58fda60e1ed7437650d75520cafb2ec
Jobega <37122818+JosephPobega@users.noreply.github.com>
2023-02-28 16:03:22 -0500

    Update and rename Main.java to Driver.java

---
+++ COMP-228/ListsandMoreLists/CovidDataProject/Driver.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+
+public class Driver {
+	public static void main(String[] args) throws IOException {
+		filereader filereader1 = new filereader();
+		
+		filereader1.fileMethod();
+		
+		
+		
+		
+	}
+  
+  
+}
