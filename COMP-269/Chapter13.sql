0368253df323246c889d0054bef4c997287db0d6
Jobega <37122818+JosephPobega@users.noreply.github.com>
2023-04-11 15:44:52 -0400

    Update Chapter13.sql

--- COMP-269/Chapter13.sql
+++ COMP-269/Chapter13.sql
@@ -5,18 +5,15 @@
 
 #1
 DROP PROCEDURE IF EXISTS test;
-
 DELIMITER //
-
 CREATE PROCEDURE test()
 BEGIN
+
 	DECLARE countOfAllRows INT;
-    
     SELECT COUNT(*)
 		INTO countOfAllRows
     FROM invoices
     WHERE (invoice_total - payment_total - credit_total) >= 5000;
-    
     SELECT CONCAT(countOfAllRows, " Invoices exceed $5,000") AS message;
     
 END//
@@ -24,3 +21,30 @@ END//
 DELIMITER ;
 
 CALL test();
+
+
+#2
+DROP PROCEDURE IF EXISTS test2;
+DELIMITER //
+CREATE PROCEDURE test2()
+BEGIN
+
+	DECLARE invoice_count INT;
+    DECLARE balance_SUM DECIMAL(9,2);
+    SELECT COUNT(*),
+		SUM(invoice_total - payment_total - credit_total)
+		INTO invoice_count, balance_sum
+        FROM invoices
+        WHERE invoice_total - payment_total - credit_total > 0;
+		
+        IF balance_sum >= 30000 THEN
+			SELECT invoice_count, balance_sum;
+		ELSE 
+			SELECT "Total balance due is less than $30,000" AS message;
+        END IF;
+    
+END//
+
+DELIMITER ;
+
+CALL test2();
