9c9c94049e0e04a123665f79ac923698dc022c38
Jobega <37122818+JosephPobega@users.noreply.github.com>
2023-04-18 14:24:41 -0400

    Update Chapter13.sql

--- COMP-269/Chapter13.sql
+++ COMP-269/Chapter13.sql
@@ -48,3 +48,51 @@ END//
 DELIMITER ;
 
 CALL test2();
+
+
+#4
+
+call test4();
+DROP PROCEDURE IF EXISTS test4;
+
+DELIMITER //
+CREATE PROCEDURE test()
+BEGIN
+	DECLARE row_not_found TINYINT DEFAULT FALSE;
+    
+    DECLARE name VARCHAR(50);
+    DECLARe inv_number VARCHAR(50);
+    DECLARE balance_due_var DECIMAL(9,2);
+    DECLARE output_string VARCHAR(1000);
+    
+    DECLARE vendor_balance CURSOR FOR
+    SELECT vendor_name, invoice_number,
+    (invoice_total - payment_total - credit_total)
+    
+    FROM vendors NATURAL JOIN invoices
+    WHERE invoice_total - payment_total - credit_total > 5000
+    ORDER BY 3 DESC;
+    
+    DECLARE CONTINUE HANDLER FOR NOT FOUND
+    SET row_not_found = TRUE;
+    SET output_string="";
+    OPEN vendor_balance;
+    
+    WHILE row_not_found = FALSE DO
+		FETCH vendor_open
+			INTO name_var, inv_number, balance_due_var;   
+    
+		SET output_string = CONCAT(output_string, balance_due_var, "|", inv_number, "|", name_var, "//");
+            
+    END WHILE;
+    
+    CLOSE vendor_balance;
+    
+    SELECT output_string AS result;
+    
+    
+END//
+
+DELIMITER ;
+
+CALL test(4);
