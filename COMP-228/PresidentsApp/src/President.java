ce1b9b5ae36b8b5189e83e5900aea6aeb0b8b4db
Jobega <37122818+JosephPobega@users.noreply.github.com>
2023-11-05 16:29:38 -0500

    Add files via upload

--- COMP-228/PresidentsApp/src/President.java
+++ COMP-228/PresidentsApp/src/President.java
@@ -1,35 +1,70 @@
 public class President {
+    private int number;
     private String name;
-    private String birthDate;
-    private String deathDate;
-    private String politicalParty;
-    private double netWorth;
+    private String birth;
+    private String death;
+    private String netWorth;
+    private String health;
 
-    public President(String name, String birthDate, String deathDate, String politicalParty, double netWorth) {
+    public President(int number, String name, String birth, String death, String netWorth, String health) {
+        this.number = number;
         this.name = name;
-        this.birthDate = birthDate;
-        this.deathDate = deathDate;
-        this.politicalParty = politicalParty;
+        this.birth = birth;
+        this.death = death;
         this.netWorth = netWorth;
+        this.health = health;
+    }
+
+    public int getNumber() {
+        return number;
     }
 
     public String getName() {
         return name;
     }
 
-    public String getBirthDate() {
-        return birthDate;
+    public String getBirth() {
+        return birth;
     }
 
-    public String getDeathDate() {
-        return deathDate;
+    public String getDeath() {
+        return death;
     }
 
-    public String getPoliticalParty() {
-        return politicalParty;
-    }
-
-    public double getNetWorth() {
+    public String getNetWorth() {
         return netWorth;
+    }
+
+    public String getHealth() {
+        return health;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBirth(String birth) {
+        this.birth = birth;
+    }
+
+    public void setDeath(String death) {
+        this.death = death;
+    }
+
+    public void setNetWorth(String netWorth) {
+        this.netWorth = netWorth;
+    }
+
+    public void setHealth(String health) {
+        this.health = health;
+    }
+
+    @Override
+    public String toString() {
+        return number + " - " + name + ", " + birth + " - " + death;
     }
 }
